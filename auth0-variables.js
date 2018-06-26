11843e3018e40ec51788428477f33402c26f4576
John Gateley <John.Gateley@Auth0.com>
2018-06-26 10:46:38 -0500

    First commit

---
+++ auth0-variables.js
@@ -0,0 +1,4 @@
+var AUTH0_CLIENT_ID='ylvEEiKUSbMxyFMvyVB8NXRQKX48jlpQ';
+var AUTH0_DOMAIN='gateley.auth0.com';
+//var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CALLBACK_URL='http://localhost:4000/';
